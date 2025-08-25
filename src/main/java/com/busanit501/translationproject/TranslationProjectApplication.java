1fa60a116a48ef0c630137819753000aae2ba948
yoojiseong <yujiseong588@gmail.com>
2025-08-25 14:05:00 +0900

    first commit

---
+++ src/main/java/com/busanit501/translationproject/TranslationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.busanit501.translationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslationProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/busanit501/translationproject/TranslationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busanit501.translationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
