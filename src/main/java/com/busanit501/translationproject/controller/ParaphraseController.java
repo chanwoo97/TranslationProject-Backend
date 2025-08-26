c414a6eb59a9011f0d5efc16cef39729c8ec054a
tlsdn741621 <tlsdn741621@gmail.com>
2025-08-26 16:27:00 +0900

    의역 화면 연동 작업 수정

--- src/main/java/com/busanit501/translationproject/controller/ParaphraseController.java
+++ src/main/java/com/busanit501/translationproject/controller/ParaphraseController.java
@@ -1,11 +1,15 @@
 package com.busanit501.translationproject.controller;
 
 import com.busanit501.translationproject.dto.ParaphraseRequest;
+import com.busanit501.translationproject.dto.ParaphraseResponse;
 import com.busanit501.translationproject.service.ParaphraseService;
+import lombok.extern.log4j.Log4j2;
 import org.json.JSONException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Log4j2
 @RequestMapping("/api/paraphrase")
 public class ParaphraseController {
 
@@ -24,14 +28,25 @@ public class ParaphraseController {
 
     // POST 요청: 요청 본문(body)에 데이터 생성
     @PostMapping
-    public String paraphrasePost(@RequestBody ParaphraseRequest request) throws JSONException {
+    public ResponseEntity<ParaphraseResponse> paraphrasePost(@RequestBody ParaphraseRequest request) throws JSONException {
         String text = request.getText();
         String language = (request.getLanguage() != null && !request.getLanguage().isEmpty()) ? request.getLanguage() : "ko";
 
         // TODO: 여기에 데이터베이스에 요청 기록을 저장하는 로직을 추가할 수 있습니다.
 
-        return paraphraseService.paraphrase(text, language);
+//        return paraphraseService.paraphrase(text, language);
+
+        log.info("ParaphraseController - 프론트에서 보낸 text : " + text);
+        log.info("ParaphraseController - 프론트에서 보낸 language : " + language);
+        String paraphrasedText = paraphraseService.paraphrase(text, language);
+
+        // 응답 DTO 생성
+        ParaphraseResponse response = new ParaphraseResponse(paraphrasedText);
+
+        // ResponseEntity.ok()를 통해 JSON 형태로 응답
+        return ResponseEntity.ok(response);
     }
+
 
     // DELETE 요청: URL 경로의 ID를 사용하여 특정 데이터 삭제
     @DeleteMapping("/{id}")
