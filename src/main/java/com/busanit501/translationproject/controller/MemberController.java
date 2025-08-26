e5cb0e0d027d5944a3e6edd3f6785594b665a3ae
yoojiseong <yujiseong588@gmail.com>
2025-08-26 14:32:54 +0900

    토큰을 이용한 자동 로그인 구현

--- src/main/java/com/busanit501/translationproject/controller/MemberController.java
+++ src/main/java/com/busanit501/translationproject/controller/MemberController.java
@@ -1,15 +1,22 @@
 package com.busanit501.translationproject.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/member")
-@RequiredArgsConstructor
+@RequestMapping("/api/member")
 @Log4j2
 public class MemberController {
 
-
+    @Tag(name = "멤버 토큰 테스트", description = "멤버 토큰 활성화 테스트")
+    @GetMapping("/me")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    public Boolean me(){
+        return true;
+    }
 }
