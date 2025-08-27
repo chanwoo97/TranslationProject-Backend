0200d35bc6e57cd7673dd6ad159a3d1e4095e7a4
minjuhyun1999 <minjoohyun67@naver.com>
2025-08-27 10:57:04 +0900

    feat: 사용자 프로필 API 구현 및 멤버 서비스 통합 사용자 정보 조회 기능 추가: memberId로 사용자를 찾을 수 있는 findByMemberId 메서드를 **MemberRepository**에 추가했습니다.
    서비스 계층 로직 도입: MemberService에 getMember 메서드를 구현하여 memberId를 통해 **MemberDTO**를 가져오도록 했습니다.
    
    API 엔드포인트 수정: /api/member/me 엔드포인트가 단순히 성공 여부만 반환하는 대신, 이제는 로그인된 사용자의 **MemberDTO**를 반환하여 프론트엔드에서 사용자 이름 등 상세 정보를 사용할 수 있게 되었습니다.

--- src/main/java/com/busanit501/translationproject/repository/MemberRepository.java
+++ src/main/java/com/busanit501/translationproject/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package com.busanit501.translationproject.repository;
 import com.busanit501.translationproject.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, String> {
     boolean existsByMemberId(String memberId);
+    Optional<Member> findByMemberId(String memberId);
 }
